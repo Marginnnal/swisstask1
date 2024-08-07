926ee03c67fd42e87c25a455a14eaa2343641c60
Marginnnal <89124644+Marginnnal@users.noreply.github.com>
2024-08-08 00:07:17 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: ["0x2c0f6113026697cb413cd12bd60b90a9a4ac273d5d5441d796291b6d910986b3"], //Your private key starting with "0x"
+      accounts: ["0x202491b6d910986b3"], //Your private key starting with "0x"
     },
   },
 };
